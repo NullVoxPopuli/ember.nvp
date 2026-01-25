b6bb13043c7c2184ce72b2e58d5ae1b4acef1e2f
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-01-25 11:59:13 -0500

    Format

--- src/layers/renovate/index.js
+++ src/layers/renovate/index.js
@@ -1,7 +1,7 @@
-import { files } from 'ember-apply';
-import { existsSync } from 'node:fs';
-import { mkdir } from 'node:fs/promises';
-import { join } from 'node:path';
+import { files } from "ember-apply";
+import { existsSync } from "node:fs";
+import { mkdir } from "node:fs/promises";
+import { join } from "node:path";
 
 /**
  * @type {import('#types').Layer}
@@ -14,26 +14,28 @@ export default {
       return;
     }
 
-    let source = project.type === "app" ? join(import.meta.dirname, 'files/app.json5') 
-    : join(import.meta.dirname, 'files/library.json5');
-    
-    let destination = join(project.directory, '.github/renovate.json5');
+    let source =
+      project.type === "app"
+        ? join(import.meta.dirname, "files/app.json5")
+        : join(import.meta.dirname, "files/library.json5");
 
-    await mkdir(join(project.directory, '.github'), { recursive: true });
+    let destination = join(project.directory, ".github/renovate.json5");
+
+    await mkdir(join(project.directory, ".github"), { recursive: true });
     await files.copyFileTo(destination, { source });
   },
 
   async isSetup(project) {
     return hasRenovateConfig(project);
-  }
+  },
 };
 
 /**
- * 
- * @param {import('#utils/project.js').Project} project 
- * @returns 
+ *
+ * @param {import('#utils/project.js').Project} project
+ * @returns
  */
 function hasRenovateConfig(project) {
-  let destination = join(project.directory, '.github/renovate.json5');
+  let destination = join(project.directory, ".github/renovate.json5");
   return existsSync(destination);
 }
