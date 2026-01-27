052eec2378566f0054cfbb4a569cc0b6d6a9fc6a
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-01-27 00:35:10 -0500

    More tests

--- src/layers/github-actions/app.test.ts
+++ src/layers/github-actions/app.test.ts
@@ -1,12 +1,12 @@
 import { beforeAll, describe, it, expect as hardExpect, afterAll } from "vitest";
-import { generate, permutate, bases, layers, reapply } from "#test-helpers";
+import { generate, layers, reapply } from "#test-helpers";
 
 import type { Project } from "ember.nvp";
 import { rm, readFile } from "node:fs/promises";
 
 const expect = hardExpect.soft;
 
-let githubActionsLayer = layers.find((layer) => layer.name === "github-actions");
+let githubActionsLayer = layers.find((layer) => layer.name === "github-actions")!;
 
 for (let packageManager of ["pnpm", "npm"] as const) {
   describe(packageManager, () => {
@@ -25,7 +25,7 @@ for (let packageManager of ["pnpm", "npm"] as const) {
     });
 
     it("did not emit a CI.yml, because it wouldn't be used", async () => {
-      let result = await githubActionsLayer!.isSetup(project);
+      let result = await githubActionsLayer.isSetup(project);
 
       expect(await project.read(".github/workflows/ci.yml")).toMatchInlineSnapshot(`undefined`);
       expect(result).toBe(true);
@@ -34,7 +34,7 @@ for (let packageManager of ["pnpm", "npm"] as const) {
     it("after emitting with an eslint layer", async () => {
       await reapply(project, ["eslint-bundled-nvp", "github-actions"]);
 
-      let result = await githubActionsLayer!.isSetup(project);
+      let result = await githubActionsLayer.isSetup(project);
 
       expect(result).toBe(true);
     });
@@ -48,6 +48,12 @@ for (let packageManager of ["pnpm", "npm"] as const) {
         expect(ciYamlContent).toContain("wyvox/action-setup-pnpm");
         expect(ciYamlContent).toContain("pnpm lint");
         expect(ciYamlContent).not.toContain(" npm lint");
+        expect(await githubActionsLayer.isSetup(project, true)).toMatchInlineSnapshot(`
+          {
+            "isSetup": true,
+            "reasons": [],
+          }
+        `);
       }
 
       if (packageManager === "npm") {
@@ -55,6 +61,12 @@ for (let packageManager of ["pnpm", "npm"] as const) {
         expect(ciYamlContent).toContain(" npm install");
         expect(ciYamlContent).toContain(" npm run lint");
         expect(ciYamlContent).not.toContain("pnpm lint");
+        expect(await githubActionsLayer.isSetup(project, true)).toMatchInlineSnapshot(`
+          {
+            "isSetup": true,
+            "reasons": [],
+          }
+        `);
       }
     });
   });
