185e7737aaae899700c84840ab39a5dbe84cefde
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-01-26 23:28:56 -0500

    More test

--- src/layers/typescript/app.test.ts
+++ src/layers/typescript/app.test.ts
@@ -1,12 +1,12 @@
 import { beforeAll, describe, it, expect as hardExpect, afterAll } from "vitest";
-import { generate, permutate, bases, layers, reapply } from "#test-helpers";
+import { generate, layers, reapply } from "#test-helpers";
 
 import type { Project } from "ember.nvp";
-import { rm, readFile } from "node:fs/promises";
+import { rm } from "node:fs/promises";
 
 const expect = hardExpect.soft;
 
-let githubActionsLayer = layers.find((layer) => layer.name === "github-actions");
+let githubActionsLayer = layers.find((layer) => layer.name === "github-actions")!;
 
 describe("starting with javascript", () => {
   let project: Project;
@@ -26,12 +26,11 @@ describe("starting with javascript", () => {
   it("applying typescript causes no changes", async () => {
     await reapply(project, ["typescript"]);
 
-    let result = await githubActionsLayer!.isSetup(project);
+    let result = await githubActionsLayer.isSetup(project);
 
     expect(result).toBe(true);
     expect(await project.gitHasDiff()).toBe(false);
-    expect(await project.hasFile("tsconfig.json")).toBe(true);
-    expect(await project.gitLastCommitMessage()).toContain("typescript");
+    expect(project.hasFile("tsconfig.json")).toBe(true);
   });
 });
 
@@ -53,10 +52,38 @@ describe("starting with typescript", () => {
   it("reapplying typescript causes no changes", async () => {
     await reapply(project, ["typescript"]);
 
-    let result = await githubActionsLayer!.isSetup(project);
+    let result = await githubActionsLayer.isSetup(project);
 
     expect(result).toBe(true);
     expect(await project.gitHasDiff()).toBe(false);
-    expect(await project.hasFile("tsconfig.json")).toBe(true);
+    expect(project.hasFile("tsconfig.json")).toBe(true);
+  });
+});
+
+describe("package.json scripts", () => {
+  let project: Project;
+
+  beforeAll(async () => {
+    project = await generate({
+      type: "app",
+      packageManager: "pnpm",
+      layers: ["typescript", "git"],
+    });
+  });
+
+  afterAll(async () => {
+    await rm(project.directory, { recursive: true, force: true });
+  });
+
+  it("type checking works", async () => {
+    let { exitCode } = await project.run("pnpm lint:types");
+
+    expect(exitCode).toBe(0);
+  });
+
+  it("build works", async () => {
+    let { exitCode } = await project.run("pnpm build");
+
+    expect(exitCode).toBe(0);
   });
 });
