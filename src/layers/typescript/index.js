65291b668bf7ffc6b891f6df97310aec5ae2eb67
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-01-26 23:48:39 -0500

    Update package.json

--- src/layers/typescript/index.js
+++ src/layers/typescript/index.js
@@ -1,12 +1,22 @@
-import { js } from "ember-apply";
+import { js, packageJson } from "ember-apply";
 import { join } from "node:path";
 import { cp } from "node:fs/promises";
 import { existsSync } from "node:fs";
 import { hasConfiguredTSBabel, prependPlugin } from "#utils/babel.js";
+import { getLatest } from "#utils/npm.js";
 
 const bases = join(import.meta.dirname, "../../bases");
 const appBase = join(bases, "minimal-app/files");
 const libraryBase = join(bases, "minimal-library/files");
+
+const deps = {
+  "@babel/plugin-transform-typescript": "^7.28.5",
+  "@ember/app-tsconfig": "^2.0.0",
+  "@glint/ember-tsc": "^1.0.8",
+  "@glint/template": "^1.7.3",
+  "@glint/tsserver-plugin": "^2.0.8",
+  typescript: "^5.9.3",
+};
 
 /**
  * @type {import('#types').Layer}
@@ -22,6 +32,7 @@ export default {
      * if tsconfig exists,
      */
     addTSConfig(project);
+    updatePackageJson(project);
     updateBabelConfig(project);
   },
 
@@ -52,6 +63,22 @@ export default {
       reasons.push(`babel.config is missing @babel/plugin-transform-typescript`);
     }
 
+    let manifest = await packageJson.read(project.directory);
+
+    if (!manifest.scripts?.["lint:types"]) {
+      if (!explain) return false;
+
+      reasons.push(`package.json is missing the "lint:types" script`);
+    }
+
+    for (let dep of Object.keys(deps)) {
+      if (!manifest.devDependencies?.[dep]) {
+        if (!explain) return false;
+
+        reasons.push(`package.json is missing ${dep} in devDependencies`);
+      }
+    }
+
     if (explain) {
       return {
         isSetup: reasons.length === 0,
@@ -62,6 +89,21 @@ export default {
     return reasons.length === 0;
   },
 };
+
+/**
+ * @param {import('#utils/project.js').Project} project
+ */
+async function updatePackageJson(project) {
+  await packageJson.modify(async (json) => {
+    json.scripts ||= {};
+    json.devDependencies ||= {};
+
+    Object.assign(json.scripts, {
+      "lint:types": "ember-tsc --noEmit",
+    });
+    Object.assign(json.devDependencies, getLatest(deps));
+  }, project.directory);
+}
 
 /**
  * @param {import('#utils/project.js').Project} project
@@ -85,6 +127,10 @@ async function addTSConfig(project) {
  * @param {import('#utils/project.js').Project} project
  */
 async function updateBabelConfig(project) {
+  if (await hasConfiguredTSBabel(project)) {
+    return;
+  }
+
   await prependPlugin(
     project,
     `[
