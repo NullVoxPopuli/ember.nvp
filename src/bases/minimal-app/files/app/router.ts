eac61ee718e7e93686978f2854b1fe5e40e918ba
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-01-28 00:10:44 -0500

    Lint:fix

--- src/bases/minimal-app/files/app/router.ts
+++ src/bases/minimal-app/files/app/router.ts
@@ -38,14 +38,14 @@ Router.map(function () {});
  * - https://github.com/NullVoxPopuli/limber/blob/67e2f54bbe224052e38f9a9e566d704411e65e86/apps/repl/app/router.ts#L35
  */
 // (window as any)._embroiderRouteBundles_ = [
-  // bundle("docs", () => [import("./templates/docs.gts")]),
-  // bundle("docs.repl-sdk", () => [import("./templates/docs/repl-sdk.gts")]),
-  // bundle("docs.ember-repl", () => [import("./templates/docs/ember-repl.gts")]),
-  // bundle("docs.embedding", () => [import("./templates/docs/embedding.gts")]),
-  // bundle("docs.editor", () => [import("./templates/docs/editor.gts")]),
-  // bundle("docs.whatever", () => [
-  //   import("./the/template.gts"),
-  //   import("./the/route.ts"),
-  //   import("./the/controller.ts"),
-  // ]),
+// bundle("docs", () => [import("./templates/docs.gts")]),
+// bundle("docs.repl-sdk", () => [import("./templates/docs/repl-sdk.gts")]),
+// bundle("docs.ember-repl", () => [import("./templates/docs/ember-repl.gts")]),
+// bundle("docs.embedding", () => [import("./templates/docs/embedding.gts")]),
+// bundle("docs.editor", () => [import("./templates/docs/editor.gts")]),
+// bundle("docs.whatever", () => [
+//   import("./the/template.gts"),
+//   import("./the/route.ts"),
+//   import("./the/controller.ts"),
+// ]),
 // ];
