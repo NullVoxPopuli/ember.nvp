66408d11dcd9b7b9cfd3c8712c6508cef6b7f8fc
Tom Carter <tommyjr@gmail.com>
2026-08-27 16:14:29 -0500

    Update eslint (+ deps) in layers

--- src/layers/eslint-bundled-nvp/index.js
+++ src/layers/eslint-bundled-nvp/index.js
@@ -19,7 +19,7 @@ export default {
     await packageJson.addDevDependencies(
       await getLatest({
         "@nullvoxpopuli/eslint-configs": "^5.5.0",
-        eslint: "^9.39.2",
+        eslint: "^10.9.1",
       }),
       project.directory,
     );
