7bf60455a06bfb8ea9ad0a0e0195ed58f5742082
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-09-10 17:41:39 -0400

    Add routes import

--- src/bases/minimal-app/files/app/app.ts
+++ src/bases/minimal-app/files/app/app.ts
@@ -15,6 +15,7 @@ import Application from "@ember/application";
 export default class App extends Application {
   modules = {
     ...import.meta.glob("./router.*", { eager: true }),
+    ...import.meta.glob("./routes/**/*", { eager: true }),
     ...import.meta.glob("./templates/**/*", { eager: true }),
     ...import.meta.glob("./services/**/*", { eager: true }),
   };
